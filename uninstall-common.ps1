4b7954365cd107ec45cb03fa7b759c53b85ef066
hophamlam <53455661+hophamlam@users.noreply.github.com>
2023-08-13 14:11:37 +0700

    Update uninstall-common.ps1

--- uninstall-common.ps1
+++ uninstall-common.ps1
@@ -1,4 +1,4 @@
 winget uninstall -e --id Microsoft.Office --silent --force	--accept-source-agreements 
-winget uninstall -e --id TeamViewer.TeamViewer --silent --force --accept-source-agreements
+winget uninstall -e --id TeamViewer.TeamViewer --silent --force --accept-source-agreements --disable-interactivity
 winget uninstall -e --id Microsoft.BingWallpaper --silent --force --accept-source-agreements
 winget uninstall -e --id VNGCorp.Zalo --silent --force --accept-source-agreements

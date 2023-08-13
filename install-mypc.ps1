c8c32bdf807951728d7e9a55a8a3b0ed22d40d7c
hophamlam <53455661+hophamlam@users.noreply.github.com>
2023-08-13 23:39:55 +0700

    Update install-mypc.ps1

--- install-mypc.ps1
+++ install-mypc.ps1
@@ -1,3 +1,8 @@
-# Check and install WSL2
-wsl --install
 winget install -e --id Twilio.Authy --force	--accept-source-agreements 
+winget install -e --id Canonical.Ubuntu.2204 --force --accept-source-agreements 
+winget install -e --id Microsoft.VisualStudioCode --force --accept-source-agreements
+winget install -e --id Notepad++.Notepad++ --force --accept-source-agreements
+winget install -e --id Microsoft.PowerToys --force --accept-source-agreements
+winget install -e --id CPUID.HWMonitor --force --accept-source-agreements
+winget install -e --id CPUID.CPU-Z --force --accept-source-agreements
+winget install -e --id WireGuard.WireGuard --force --accept-source-agreements

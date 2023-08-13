47d02c1946e4321e47d04169055a9b8d0fd5348e
hophamlam <53455661+hophamlam@users.noreply.github.com>
2023-08-13 12:36:58 +0700

    Update common.ps1

--- common.ps1
+++ common.ps1
@@ -1,4 +1,4 @@
 winget install -e --id Microsoft.Office --force	--accept-source-agreements 
 winget install -e --id TeamViewer.TeamViewer --force --accept-source-agreements
 winget install -e --id Microsoft.BingWallpaper --force --accept-source-agreements
-winget install -e --id VNGCorp.Zalo --force	--accept-source-agreements
+winget install -e --id VNGCorp.Zalo --force --accept-source-agreements
